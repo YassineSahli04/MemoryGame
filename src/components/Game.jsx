af584fe5634f52d24e431873d9dc18b2443901f6
Yassine SAHLI <ysahl091@uottawa.ca>
2025-06-21 00:02:32 -0400

    adapted the game params depending on the level

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,42 +1,54 @@
 import { useEffect, useState, useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
 import InitialCardList from "./InitialCardList";
 import './GameStyle.css'
 import CardProposition from "./CardProposition";
 import Items from "./Items";
 
 export default function Game(){
+    const [searchParams] = useSearchParams();
+    const level = searchParams.get("level") || "easy";
+
+    const [flipTimer,numberCards,startingNumberImgs] = useMemo(() => {
+        switch (level) {
+            case "medium":
+                return [1500,2,3];
+            case "hard":
+                return [1000,3,4];
+            default:
+                return [2000,2,2]; 
+        }
+    }, [level]);
+
+
     const [view, setView] = useState('cards');
-    const [imgNumber, setImgNumber] = useState(2)
+    const [imgNumber, setImgNumber] = useState(startingNumberImgs)
 
     const selectedItems = useMemo(()=>{
         console.log('yes')
         const shuffledItems = [...Items].sort(() => Math.random() - 0.5);  
         return shuffledItems.slice(0, imgNumber);
-    },[Items, imgNumber]);
+    },[ imgNumber]);
 
     const handleResult = (result) =>{
         if(result === true){
-            setView('next')
+            setImgNumber(prev => prev + 1)
+            setView('cards')
         }else{
             setView('end')
         }
     }
 
-    useEffect(()=>{
-        if(view === 'next'){
-            setImgNumber(prev => prev + 1)
-            setView('cards')
-        }
-    },[view])
+
 
     return (<>
     <div className="container">
         {view === 'cards' && (
-            <InitialCardList items={selectedItems} flipTimer={1500} onComplete={() => setView('results')}/>
+            <InitialCardList items={selectedItems} flipTimer={flipTimer} onComplete={() => setView('results')}/>
         )}
 
         {view === 'results' && (
-            <CardProposition numberCards={2} items={selectedItems} result={handleResult}/>
+            <CardProposition numberCards={numberCards} items={selectedItems} result={handleResult}/>
         )}
 
         {view === 'end' && (
